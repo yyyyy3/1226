3b3989f0f9b270760b2408c2031a705260e988d3
yyyyy3 <chloe950302@gmail.com>
2025-03-17 11:45:49 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,95 +1,52 @@
-let sprites = {
-    // 第一個角色的精靈圖（原有的）
-    player1: {
-      idle: {
-        img: null,
-        width: 165,
-        height: 188,
-        frames: 16
-      },
-      walk: {
-        img: null,
-        width: 166,
-        height: 177,
-        frames: 6
-      },
-      
-      jump: {
-        img: null,
-        width: 228,
-        height: 177,
-        frames: 10
-      }
-    },
-    // 第二個角色的精靈圖
-    player2: {
-      idle: {
-        img: null,
-        width: 88,
-        height: 191,
-        frames: 8
-      },
-      walk: {
-        img: null,
-        width: 177,
-        height: 177,
-        frames: 11
-      },
-      jump: {
-        img: null,
-        width: 221,
-        height: 185,
-        frames: 12
-      }
-    },
-     explosion: {  //爆炸圖
-      img: null,
-      width: 133,
-      height: 100,
-      frames: 4
-    },
-    bullet: {  //發射子彈
-      img: null,
-      width: 70,
-      height: 46,
-      frames: 8
-    }
-  };
-  function preload() {
-    // 載入背景圖片
-    bgImage = loadImage('ocean.jng');
-  
-  // 載入角色1的圖片
-  sprites.player1.idle.img = loadImage('5.png');
-  sprites.player1.walk.img = loadImage('7.png');
-  sprites.player1.jump.img = loadImage('11.png');
-  
-  // 載入角色2的圖片
-  sprites.player2.idle.img = loadImage('9.png');
-  sprites.player2.walk.img = loadImage('8.png');
-  sprites.player2.jump.img = loadImage('10.png');
-  
+let seaGrass = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // 設置畫布背景透明
+  clear();
+  // 創建海草物件
+  for (let i = 0; i < 30; i++) { // 增加到 30 條線
+    seaGrass.push(new SeaGrass(random(width), height));
+  }
+}
+
+function draw() {
+  clear(); // 清除畫布，保持背景透明
+
+  // 繪製海草
+  for (let i = 0; i < seaGrass.length; i++) {
+    seaGrass[i].update();
+    seaGrass[i].display();
+  }
+}
+
+// 定義海草類別
+class SeaGrass {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.angle = 0; // 初始角度為0
+    this.length = 10 * 37.795275591; // 海草的長度，50公分轉換為像素
+    this.amplitude = random(1, 3); // 輕微波動幅度
+    this.frequency = random(0.01, 0.03); // 波動頻率
   }
 
-  
-  
-  function setup() {
-    createCanvas(800, 600);
-    frameRate(30);
+  update() {
+    // 改變海草隨時間波動的角度
+    this.angle = sin(frameCount * this.frequency) * this.amplitude;
   }
-  
-  function draw() {
-    background(220);
-    
-    // 繪製角色1
-    drawCharacter(player1, sprites.player1);
-    
-    // 繪製角色2
-    drawCharacter(player2, sprites.player2);
-    
-    // 更新動畫幀
-    if (frameCount % 5 === 0) {
-      updateAnimation(player1, sprites.player1);
-      updateAnimation(player2, sprites.player2);
-    }
+
+  display() {
+    push();
+    translate(this.x, this.y); // 將繪製原點移到海草的基點
+    rotate(this.angle); // 根據角度旋轉
+    stroke(34, 139, 34, 150); // 海草顏色，帶透明度
+    strokeWeight(10); // 設定線條寬度為原來的五倍
+    line(0, 0, 0, -this.length); // 畫海草
+    pop();
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
